6aacdb3645296d77d23a23d8e46855f96c97af76
Gaurav Ghuge <guduughuge7@gmail.com>
2024-08-31 16:43:58 +0530

    first commit

---
+++ day1_init/test.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+   
+   // cout<< "Welcome to CPP World For New Start ";
+
+
+   int a = 20;
+   int b = 25;
+
+   for(int i=a; i<= a*b; i++) {
+
+      if(i%a==0 && i%b==0) {
+
+         // cout<<i;  // print the i
+
+         printf("%d", i);
+
+
+         break;
+
+      }
+   }
+
+
+   return 10;
+
+
+}
+
+
+// check a number perfect square number or not 
+
+// check a number perfect cube number or not 
