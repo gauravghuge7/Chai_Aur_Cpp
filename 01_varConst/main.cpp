5c5fd63aaa275270911f51e88f673b4a31bc9891
Gaurav Ghuge <guduughuge7@gmail.com>
2024-09-07 13:28:27 +0530

    create the code git push

---
+++ 01_varConst/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+
+   const int gaurav_balance = 13000;
+
+
+
+   // gaurav_balance = 20000;   /// not modifable because declare as const 
+
+
+   
+
+
+   cout <<" hello from gaurav ghuge" <<endl;
+
+}
